b43fedee66a8ac012436d72aad639e0ddc34406a
shambhavi-tiwari <shambhavitiwari@Shambhavi-T.local>
2024-10-25 23:08:42 +0530

    -newversionshambhavi-

--- components/header.tsx
+++ components/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-4 hidden md:flex">
+        <div className="mr-3 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="hidden font-bold sm:inline-block">Gradient Goods</span>
           </Link>
